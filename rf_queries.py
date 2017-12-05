6df8dd43c8a6c2ac1f8ea95422cadba77547677e
Billy J Hamlin <billy.hamlin@gmail.com>
2017-12-05 09:20:30 -0500

    Updated query to order by time

--- rf_queries.py
+++ rf_queries.py
@@ -7,6 +7,6 @@ SELECT sono AS order_id, data_point, value, '2017-11-28 14:00:00.000000' as dts
 SELECT cxid AS order_id, data_point, value, brec_ts as dts
   FROM connixt.adhoc_data 
   WHERE order_type = 'AMI Change Out' 
-ORDER  BY order_id, data_point
+ORDER  BY dts
 
 '''.strip()
